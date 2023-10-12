31b0231f21258f5cc4c9c2fc825c4a4114e4024d
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-12 18:05:04 +0600

    Task7

---
+++ Tyuiu.SugrovskiyNI.Sprint2.Task7.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Sprint2.Task7.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Sprint2.Task7.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c140966-d4d5-4402-b51e-eb77a47e39e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SugrovskiyNI.Sprint2.Task7.V15/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SugrovskiyNI.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.SugrovskiyNI.Sprint2.Task7.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Сугровский Н. И. | ИИПб-23-1";
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                 *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                         *");
+            Console.WriteLine("* Задание #7                                                                *");
+            Console.WriteLine("* Вариант #15                                                               *");
+            Console.WriteLine("* Выполнил: Сугровский Никита Игоревич | ИИПб-23-1                          *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Написать программу которая запрашивает исходные данные                    *");
+            Console.WriteLine("* вычисляет, находится ли точка с координатами X,Y в заштрихованной области *");
+            Console.WriteLine("*                                                                           *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("*****************************************************************************");
+
+            Console.Write("Введите значение переменной X  ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение переменной Y  ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
